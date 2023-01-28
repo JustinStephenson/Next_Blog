1cef43d163295b4254554a85cf48c14d191acb31
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2023-01-28 09:29:37 -0500

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import Head from 'next/head';
+import styles from '@/styles/Home.module.css';
+
+// The default exports is what Next will trigger,
+// when the route is hit in the browser
+export default function Home() {
+	return <div></div>;
+}
