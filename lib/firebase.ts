3100540c2ee268608ad122233445f0c33c8c5ed3
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2023-01-28 10:04:51 -0500

    Adding firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,1 +1,24 @@
-export default null;
+import firebase from 'firebase/compat/app';
+// Bundle SDK's to main js bundle
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyBOMEF8KUXccMTfkaqTWHPL11IpzHOAY9U',
+	authDomain: 'nextblog-55a1c.firebaseapp.com',
+	projectId: 'nextblog-55a1c',
+	storageBucket: 'nextblog-55a1c.appspot.com',
+	messagingSenderId: '244329084757',
+	appId: '1:244329084757:web:c1c33159ae5bd64788e1dc',
+};
+
+// Initialize Firebase
+if (firebase.app.length) {
+	firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import '../lib/firebase';
 
 // This App component wraps the rest of the components in the app
 export default function App({ Component, pageProps }: AppProps) {
