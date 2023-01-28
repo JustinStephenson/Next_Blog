aa3451d85c102df287011d91498ebedf7c9920a3
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2023-01-28 09:35:15 -0500

    Initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
+// This App component wraps the rest of the components in the app
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
