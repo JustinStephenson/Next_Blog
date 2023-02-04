a35d82faccd1b8de5fd3bfac8ccb8327ad87d563
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2023-02-04 13:57:37 -0500

    fixing bug

--- pages/admin/[slug].tsx
+++ pages/admin/[slug].tsx
@@ -91,14 +91,14 @@ const PostForm = ({ defaultValues, postRef, preview }: any) => {
 			)}
 
 			<div className={preview ? styles.hidden : styles.controls}>
-				<textarea name="content" ref={register}></textarea>
+				<textarea name="content" {...register('content')}></textarea>
 
 				<fieldset>
 					<input
 						className={styles.checkbox}
 						name="published"
 						type="checkbox"
-						ref={register}
+						{...register('published')}
 					/>
 					<label>Published</label>
 				</fieldset>
